e0061d0cafab207c23c34d0d01cfef2042fcbf30
Yuuki-Kishi <yuuki.k.2006@gmail.com>
2024-06-10 22:16:40 +0900

    Initial Commit

---
+++ play-tag-with-negotiations/play_tag_with_negotiationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  play_tag_with_negotiationsApp.swift
+//  play-tag-with-negotiations
+//
+//  Created by 岸　優樹 on 2024/06/10.
+//
+
+import SwiftUI
+
+@main
+struct play_tag_with_negotiationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
